ccc35f3118eda8da9b650768b988e6febbfc5692
paulotavoraa <paulotavoraa@gmail.com>
2021-11-05 22:13:54 +0100

    Add project files.

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using SQLitePCL;
+
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=mydb.db");
+            SQLitePCL.Batteries.Init();
+        }
+
+    }
+}
